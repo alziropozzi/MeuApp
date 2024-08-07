1d920befdf6f62a8031330df545ab8c92692ca05
Alziro Pozzi <alziropozzi@hotmail.com>
2024-08-07 16:29:18 -0400

    Adicionei signIn signUp e o appwrite

---
+++ context/GlobalProvider.js
@@ -0,0 +1,47 @@
+import { createContext, useContext, useState, useEffect, Children } from "react";
+
+import { getCurrentUser } from "../lib/appwrite";
+
+const GlobalContext = createContext();
+export const useGlobalContext = () => useContext (GlobalContext);
+
+const GlobalProvider = ({ children }) => {
+    const [isLoggedIn, setisLoggedIn] = useState(false);
+    const [user, setUser] = uUeState(null);
+    const [isLoading, setisLoading] = useState(true);
+
+    useEffect(() =>{
+        getCurrentUser()
+        .then((res) => {
+            if(res){
+                setisLoggedIn(true);
+                setUser(res)
+            } else {
+                setIsLoggedIn(false)
+                setUser(null)
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+        .finally(() =>{
+            setisLoading(false)
+        })
+    }, []);
+
+    return(
+        <GlobalContext.Provider
+            value={{
+                isLoggedIn,
+                setIsLoggedIn,
+                user,
+                setUser,
+                isLoading
+            }}
+        >
+            {children}
+        </GlobalContext.Provider>
+    )
+}
+
+export default GlobalProvider;
